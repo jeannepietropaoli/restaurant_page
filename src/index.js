324b8043c6aef7d1eeb76177724d9201df51f4bc
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-06-29 22:48:53 +0200

    index.js Refacto

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import {HOMEPAGE_ELEMENTS} from './initPageLoad';
 import {addVolet} from './volet';
 import { displayMenu } from './menu';
 import {CONTENT} from './volet';
 import {volet} from './volet';
 import {MAIN_ELEMENTS} from './initPageLoad';
 
-console.log(Array.from(CONTENT.children).some(child => child.classList.contains('main')));
+const navElements = (() => {
+    return {
+        home : document.querySelector('li:nth-of-type(1)'),
+        menu : document.querySelector('li:nth-of-type(2)'),
+        contact : document.querySelector('li:nth-of-type(3)'),
+    }
+})()
 
-const home = document.querySelector('li:nth-of-type(1)');
-const menu = document.querySelector('li:nth-of-type(2)');
-const contact = document.querySelector('li:nth-of-type(3)');
-
-let currentScetion = "Home";
-function isNotCurrentSection (elementClicked){
-    return elementClicked !== currentScetion;
-}
+const navSection = (()=> {
+    let currentSection = 'Home';
+    const setCurrentSection = (event)=> {
+        currentSection = event.target.innerText;
+    }
+    const isNotCurrentSection = (elementClicked)=> {
+        return elementClicked !== currentSection;
+    }
+    return {
+        setCurrentSection,
+        isNotCurrentSection
+    }
+})()
 
 const removeMainSection = ()=> {
     if (Array.from(CONTENT.children).some(child => child.classList.contains('main'))){
@@ -33,43 +43,28 @@ const rollWhiteDiv = ()=> {
         setTimeout(()=> {document.querySelector('.visible').style.height = '70%'}, 1000);
 }
 
-menu.addEventListener('click', function(e) {
-    console.log(currentScetion);
-    if (isNotCurrentSection(e.target.innerText)){
-        if (document.querySelector('.main')) removeMainSection();
-        if (document.querySelector('.visible')){
-            rollWhiteDiv();
-            setTimeout(()=> {displayMenu.create()}, 1000);
-        }
-        else {
-            document.querySelector('.volet').style.height = '70%';
-            displayMenu.create();
-        }
-        addVolet.makeVoletVisible();
+function displayContent(){
+    if (document.querySelector('.main')) removeMainSection();
+    if (document.querySelector('.visible')){
+        rollWhiteDiv();
+        setTimeout(()=> {displayMenu.create()}, 1000);
     }
-    currentScetion = 'Menu';
-});
-
-contact.addEventListener('click', function(e) {
-    console.log(currentScetion);
-    if (isNotCurrentSection(e.target.innerText)){
-        if (document.querySelector('.main')) removeMainSection();
-        if (document.querySelector('.visible')){
-            rollWhiteDiv();
-            setTimeout(()=> {displayMenu.create()}, 1000);
-        }
-        else {
-            document.querySelector('.volet').style.height = '70%';
-            displayMenu.create();
-        }
-        addVolet.makeVoletVisible();
+    else {
+        document.querySelector('.volet').style.height = '70%';
+        displayMenu.create();
     }
-    currentScetion = 'Contact';
-});
+    addVolet.makeVoletVisible();
+}
 
-home.addEventListener('click', function(e) {
-    console.log(currentScetion);
-    if (isNotCurrentSection(e.target.innerText)){
+for (let i=1; i<=2; i++){
+    Object.values(navElements)[i].addEventListener('click', function(e) {
+        if (navSection.isNotCurrentSection(e.target.innerText)) displayContent();
+        navSection.setCurrentSection(e);
+    });
+}
+
+navElements.home.addEventListener('click', function(e) {
+    if (navSection.isNotCurrentSection(e.target.innerText)){
         if (document.querySelector('.main')) removeMainSection();
         if (document.querySelector('.visible')){
             document.querySelector('.visible').style.height = '0%';
@@ -77,7 +72,7 @@ home.addEventListener('click', function(e) {
         }
         setTimeout(()=> {MAIN_ELEMENTS.display()}, 100);
     }
-    currentScetion = 'Home';
+    navSection.setCurrentSection(e);
 });
 
 
